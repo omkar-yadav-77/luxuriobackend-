2044a42cd0235449db35393daf76702d0ea4e72d
omkar-yadav-77 <miomkar77@gmail.com>
2026-01-09 21:34:29 +0530

    update backend

---
+++ src/main/java/com/automart/backend/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.automart.backend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Backend Running";
+    }
+}
